19b728d44ea93f031e6ba2bb721aeadb3b990010
Morelaa <morelamjs322@gmail.com>
2026-09-16 08:50:00 +0700

    Upload via WhatsApp Bot

--- Library/stickerPackHelper.js
+++ Library/stickerPackHelper.js
@@ -1,3 +1,9 @@
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 import crypto from 'crypto';
 import https from 'https';
 import JSZip from 'jszip';
