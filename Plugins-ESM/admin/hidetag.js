b7fdcf0a2572f56f5ac59b004a8ad823b75fff8b
Morelaa <morelamjs322@gmail.com>
2026-09-16 08:50:14 +0700

    Upload via WhatsApp Bot

--- Plugins-ESM/admin/hidetag.js
+++ Plugins-ESM/admin/hidetag.js
@@ -1,4 +1,9 @@
-'use strict';
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 const handler = async (m, { conn, text, participants, groupMeta }) => {
     const list = participants || groupMeta?.participants || (await conn.groupMetadata(m.chat)).participants;
     const mentions = list.map((p) => p.id);
