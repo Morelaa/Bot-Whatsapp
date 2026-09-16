9a6d9b74ff94b88ece219ce98b529d1e3e00df9f
Morelaa <morelamjs322@gmail.com>
2026-09-16 08:51:45 +0700

    Upload via WhatsApp Bot

--- System/ownerlist.js
+++ System/ownerlist.js
@@ -1,4 +1,9 @@
-'use strict';
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 import * as kv from '../Database/kvstore.js';
 import config from '../config.js';
 import { normNum } from '../Library/resolve.js';
@@ -10,7 +15,6 @@ function readExtra() {
 function writeExtra(list) {
     kv.set(KEY, list);
 }
-
 export function getExtraOwners() {
     return readExtra();
 }
