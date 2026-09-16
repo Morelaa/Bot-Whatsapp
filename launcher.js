bc24e762b827b0676579106703f4ccc352c1162b
Morelaa <morelamjs322@gmail.com>
2026-09-16 09:12:22 +0700

    Upload via WhatsApp Bot

--- launcher.js
+++ launcher.js
@@ -1,4 +1,9 @@
-'use strict';
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 import { spawn } from 'child_process';
 import path from 'path';
 import { fileURLToPath } from 'url';
