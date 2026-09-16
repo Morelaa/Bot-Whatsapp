bf4a4ec842779c9d204bfcfe281a3f5e8d183254
Morelaa <morelamjs322@gmail.com>
2026-09-16 08:51:42 +0700

    Upload via WhatsApp Bot

--- System/logger.js
+++ System/logger.js
@@ -1,4 +1,9 @@
-'use strict';
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 import pino from 'pino';
 import config from '../config.js';
 const level = config.debug ? 'debug' : config.baileysLogLevel;
