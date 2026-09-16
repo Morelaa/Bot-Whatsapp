d974f4029a914d760d5cdf11e6b29600bc86cc1e
Morelaa <morelamjs322@gmail.com>
2026-09-16 08:51:23 +0700

    Upload via WhatsApp Bot

--- Plugins-ESM/sticker/toimg.js
+++ Plugins-ESM/sticker/toimg.js
@@ -1,4 +1,9 @@
-'use strict';
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+//    </>  𝐂𝐫𝐞𝐝𝐢𝐭𝐬  </>      //
+//   𝐂𝐫𝐞𝐚𝐭𝐨𝐫: 𝐀𝐥𝐩𝐮𝐭𝐫𝐚𝐚       //
+//   𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @𝐬𝐢𝐚𝐩𝐚𝐚𝐤𝐮𝟖𝟕𝟖     //
+//﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌//
+
 import sharp from 'sharp';
 import { findMediaMessage, downloadMessageMedia } from '../../Library/handle.js';
 const handler = async (m, { conn }) => {
